5a56174b8ec94ff831eda6b88abd0b8d955dfa4b
mgberrysd <mgberrysd@gmail.com>
2024-02-20 15:28:16 -0800

    Updated models, will need to add in email validation

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    thoughts: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Thought',
+    },
+    ],
+    friends: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return this.friends.length;
+  });
+
+const User = model('user', userSchema);
+
+module.exports = User;
